2656f4a214183855a00e923b72fa19fc4f4ffdfc
Maja <skymajka@gmail.com>
2020-04-05 14:26:34 +0200

    remommended posts on the top

--- index.php
+++ index.php
@@ -4,23 +4,46 @@
     <main>
         <div class="container">
 
-            <!--Section: Dynamic Content Wrapper-->
+            <!--Section: Recommended articles-->
             <section>
-              <div id="dynamic-content"></div>
+
+                <h2 class="my-4"><?php _e('Polecam w tym miesiącu', 'neuro')?></h2>
+
+                <div class="row hm-white-slight rounded z-depth-1">
+
+                <?php
+                    $recommended = new WP_Query( ['category_name' => 'polecane'] );
+                    if ( $recommended->have_posts() ) {
+                        while ( $recommended->have_posts() ) {
+                        $recommended->the_post();
+                    ?>
+                <div class="col">
+                    <div class="my-3">
+                        <?php the_post_thumbnail( 'medium-large', array( 'class'=> 'img-fluid')); ?>
+                    </div>
+                    <h3 class="h5-responsive my-3">
+                        <a href="<?php echo get_permalink() ?>"><?php the_title(); ?></a>
+                    </h3>
+                </div>
+
+                <?php
+                    } // end while
+                    } // end if
+                ?>
+                </div>
 
             </section>
-            <!--Section: Dynamic Content Wrapper-->
+            <!-- /.Section: Recommended articles-->
 
-            <!--Section: Articles-->
+            <!--Section: Recent articles-->
             <section>
 
-                <!--Section heading-->
-                <h1 class="h2 font-weight-bold my-4">Recent articles</h1>
+                <h2 class="my-4"><?php _e('Najnowsze artykuły', 'neuro')?></h2>
 
                 <?php get_template_part( 'template-parts/posts-list' ); ?>
 
             </section>
-            <!--Section: Articles-->
+            <!--/.Section: Recent articles-->
 
         </div>
     </main>
