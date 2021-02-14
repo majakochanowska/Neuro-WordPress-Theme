226d15b371f638cf925ad12b95484a9cbe6ae7e2
majakochanowska <51491969+majakochanowska@users.noreply.github.com>
2021-02-14 12:32:17 +0100

    link to logo fixed

--- header.php
+++ header.php
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     <header class="site-header">
         <div class="container">
             <div class="row align-items-center my-2">
-                <div class="col-xs-2 ml-1"><a href="<?php esc_url( bloginfo('url') ); ?>" title="<?php esc_attr_e('Strona główna', 'neuro')?>"><?php the_custom_logo() ?></a></div>
+                <div class="col-xs-2 ml-1"><?php the_custom_logo() ?></div>
                 <div class="col-xs-10"><h1 class="font-weight-lighter ml-3"><?php esc_html( bloginfo( 'description' ) ); ?></h1></div>
             </div> 
         </div>
