8b499347563712fa1bc6395662ee79fc04916fc3
majakochanowska <51491969+majakochanowska@users.noreply.github.com>
2021-04-17 13:04:32 +0200

    gutenberg block for references section

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+import { __ } from '@wordpress/i18n';
+import { registerBlockType } from '@wordpress/blocks';
+import { InnerBlocks } from '@wordpress/block-editor';
+
+registerBlockType( 'neuro/references-block', {
+	title: __( 'Bibliografia', 'neuro' ),
+	icon: 'book',
+	category: 'text',
+
+	edit: ( props ) => {
+
+		const ALLOWED_BLOCKS = [ 'core/list', 'core/paragraph' ];
+
+		return (
+			<div className={ props.className }>
+				<h4>{ __( 'Bibliografia', 'neuro' ) }</h4>
+				<InnerBlocks allowedBlocks={ ALLOWED_BLOCKS } />
+			</div>
+		);
+	},
+	save: ( props ) => {
+		
+		return (
+			<div className={ props.className }>
+				<h4>{ __( 'Bibliografia', 'neuro' ) }</h4>
+				<InnerBlocks.Content />
+			</div>
+		);
+	},
+} );
