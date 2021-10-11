6d5948c993af489f21d3dd175fd6ce07c275af46
Maja Kochanowska <maja.kochanowska@mayeryn.com>
2021-10-11 20:33:11 +0200

    unused js removed

--- neuroskoki/functions.php
+++ neuroskoki/functions.php
@@ -13,10 +13,6 @@ function neuro_enqueue_scripts() {
   wp_enqueue_style( 'MDB', get_template_directory_uri() . '/css/mdb.min.css' );
   wp_enqueue_style( 'Googlefonts', 'https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300&display=swap' );
   wp_enqueue_style( 'Style', get_template_directory_uri() . '/style.css', array(), '1.2.3' );
-  wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/js/jquery.min.js', array(), '3.3.1', true );
-  wp_enqueue_script( 'Tether', get_template_directory_uri() . '/js/popper.min.js', array(), '1.0.0', true );
-  wp_enqueue_script( 'Bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '1.0.0', true );
-  wp_enqueue_script( 'MDB', get_template_directory_uri() . '/js/mdb.min.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'neuro_enqueue_scripts' );
 
